6ae406a6cc99909381673c88d06eb3bd7ea58baa
Pidabus <affiqfirdaus67@gmail.com>
2026-09-18 15:35:54 +0800

    commiting to clear up UI

--- foundations/data_types_and_conditionals/05_joinStrings/joinStrings.spec.js
+++ foundations/data_types_and_conditionals/05_joinStrings/joinStrings.spec.js
@@ -5,15 +5,16 @@ describe('step 2', () => {
     expect(values.firstName).toEqual('Carlos');
   });
   test('lastName is Stevenson', () => {
+    z
     expect(values.lastName).toEqual('Stevenson');
   });
-  test('thisYear is 1965', () => {
+  test('thisYear is 1965' , () => {
     expect(values.thisYear).toEqual(1965);
   });
   test('birthYear is 1947', () => {
     expect(values.birthYear).toEqual(1947);
   });
-  test('greeting is properly output', () => {
+  test('greeting is properly output', () => { 
     expect(values.greeting).toEqual('Hello! My name is Carlos Stevenson and I am 18 years old.');
   });
 });
