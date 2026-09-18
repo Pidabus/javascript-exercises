aeb7c8897a5d486b3ce2cdb77da22a0b777bc43d
Pidabus <affiqfirdaus67@gmail.com>
2026-09-18 16:28:04 +0800

    completed 01_calculator

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,26 +1,49 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length === 0 ) return 0;
+  return arr.reduce((sum, current) => sum + current);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  if (b === 0) return 1;
+  if (b === 1) return a;
+
+  let oriA = a;
+  for (let i = 1; i < b; i++) {
+    a *= oriA;
+  }
+  return a;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if ((num < 0)) {
+    console.log("Invalid input. Please enter a positive number.");
+    return;
+  }
+
+  if (num === 0) return 1;
+
+  let limit = num;
+  for (let i = 1; i < limit; i++) {
+    num *= i;
+  }
+  return num;
 };
+
+// Personal quickTest
+array = [1, 2, 3, 4, 5];
+console.log(factorial(6));
 
 // Do not edit below this line
 module.exports = {
---
+++ foundations/object_basics/01_calculator/test.js
@@ -0,0 +1,9 @@
+array = [1, 2, 3, 4, 5];
+
+// const sum = function (arr) {
+//   if (arr === ) return 0;
+//   return arr.reduce((sum, current) => sum + current);
+// };
+
+arrayEmpty = [];
+console.log(arrayEmpty);
