8b844dfb70d09bb8ec596c054114724e49858b07
Pidabus <affiqfirdaus67@gmail.com>
2026-09-21 09:04:57 +0800

    validated input so negatives will output OOPS

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,13 +1,20 @@
 let arr = [0, 1];
-for (i = 2; i <= 25; i ++) {
-    let newElem = arr[i-1] + arr[i-2];
+for (i = 2; i <= 25; i++) {
+    let newElem = arr[i - 1] + arr[i - 2];
     arr.push(newElem);
 }
 
 // console.log(arr[25]); --> Verified the array correctly made a fibonacci sequence up to the 25th element.
 
-const fibonacci = function(num) {
+const fibonacci = function (num) {
+    let tempNumber = num;
 
+    if (typeof num === "string") {
+        tempNumber = Number(tempNumber);
+    }
+    if (tempNumber < 0) {
+        return console.log("OOPS");
+    }
 };
 
 // Do not edit below this line
