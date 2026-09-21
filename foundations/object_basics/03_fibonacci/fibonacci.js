a481f83ee4a7218586d466d0208abe43aeb63692
Pidabus <affiqfirdaus67@gmail.com>
2026-09-21 08:40:51 +0800

    Made an array that generates a fiboncacci sequence up to the 25th element

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+let arr = [0, 1];
+for (i = 2; i <= 25; i ++) {
+    let newElem = arr[i-1] + arr[i-2];
+    arr.push(newElem);
+}
+
+// console.log(arr[25]); --> Verified the array correctly made a fibonacci sequence up to the 25th element.
+
+const fibonacci = function(num) {
 
 };
 
