3bb3a58de93bfb880f3f8a9972c74a464caaff6a
Pidabus <affiqfirdaus67@gmail.com>
2026-09-21 15:54:31 +0800

    completed findTheOldest

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function(people) {
-    
+const findTheOldest = function (people) {
+
+    let max = 0;
+    for (let person of people) {
+        if (person.yearOfDeath === undefined) {
+            const customDeath = new Date();
+            person.yearOfDeath = customDeath.getFullYear();
+        }
+        let age = person.yearOfDeath - person.yearOfBirth; 
+        if ((age) > max) {
+            max = (age);
+        };
+    };
+
+    for (let person of people) {
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if (age === max) return person;
+    };
 };
 
 // Do not edit below this line
